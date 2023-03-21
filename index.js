f763259768c5aa58a337c3222b31f16ab3f047a1
AJ Dalbany Garrett <ajdg8811@gmail.com>
2023-03-21 14:48:30 -0400

    add MovieList

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import movieTitle from "./movieTitle.js";
-import productionTeam from "./productionTeam.js";
+import movieTitle from "./Movie.js";
+import productionTeam from "./ProductionTeam.js";
+import MovieList from "./MovieList.js";
+
 
 let TEWWG = new movieTitle('Their Eyes Were Watching God', 'N/A', "March 6, 2005", "synopsis?", "Book-adaption",
  "Golden Globe Award for Best Performance by an Actress In A Mini-series or Motion Picture Made for Television","6.4" )  
