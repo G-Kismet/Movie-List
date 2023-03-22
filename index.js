b9c730386d2cad625c47abe0dbd770912f59302b
AJ Dalbany Garrett <ajdg8811@gmail.com>
2023-03-22 10:58:34 -0400

    change names

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ let LyricRoss = new Actor("Lyric", "Ross","various", "African-American voice act
 let TEWWG = new movieTitle('Their Eyes Were Watching God', 'n/a', "cast", "March 6, 2005", "synopsis?", "Book-adaption",
  "Golden Globe Award for Best Performance by an Actress In A Mini-series or Motion Picture Made for Television","6.4" )  
 
-let WendellAndWild = new movieTitle("Wendell & Wild", `${DarnellMartin}`, `${LyricRoss}`,"October 21, 2022",
+let WendellAndWild = new movieTitle("Wendell & Wild", DarnellMartin.firstName, LyricRoss.firstName,"October 21, 2022",
 "The two devious demon brothers Wendell and Wild have to face their arch-enemy with the help of the nun Sister Helly, who is notorious for expelling demons. However, the brothers are not only plagued by her, but also by her altar boys.", "Animation ??", "No Accolades", "6.4")
 
 
 
 //create MovieList
-let Musicals = new MovieList("Musicals")
+let Musicals = new MovieList("Musicals", ['High School Musicals', 'Polly', "Singin' In The Rain"])
 //add Movie to Musical Movie List
 Musicals.addMovie(WendellAndWild);
+console.log(Musicals)
+
 //remove Movie from Musical Movie
 Musicals.removeMovie(WendellAndWild);
-
-let FavoriteList = new MovieList("Favorite Movie List")
-FavoriteList.addMovie(TEWWG);
-
 console.log(Musicals.Movie);
-console.log(Musicals)
+
+let FavoriteList = new MovieList("Favorite Movie List", [])
+FavoriteList.addMovie(TEWWG);
 console.log(FavoriteList);
